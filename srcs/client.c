397ce8c3dcea7a50f65ed65fc9966ab80a548548
If you're my friend you know my name <70669920+m4r11@users.noreply.github.com>
2021-10-29 23:01:41 +0100

    yay

--- srcs/client.c
+++ srcs/client.c
@@ -6,50 +6,34 @@
 /*   By: user <mvaldeta@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 19:06:22 by user              #+#    #+#             */
-/*   Updated: 2021/10/27 15:37:56 by user             ###   ########.fr       */
+/*   Updated: 2021/10/29 22:40:58 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "/Users/mvaldeta/42/42Cursus/02.minitalk/includes/minitalk.h"
 
-#include <signal.h>
-#include <unistd.h>
-
-static int	send_ascii(pid_t pid, char c)
+int sender(int pid, char *message)
 {
-	int	bit;
-
-	bit = 7;
-	while (--bit > -1)
-	{
-		if (c & (1 << bit))
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(1000);
-	}
-	return (0);
-}
-
-static int	sender(pid_t pid_server, char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] >= 32 && str[i] <= 126)
-	{
-		if (send_ascii(pid_server, str[i]))
-			return (1);
-		++i;
-	}
-	i = 0;
-	while (i < 7)
-	{
-		kill(pid_server, SIGUSR1);
-		usleep(1000);
-		i++;
-	}
-	return (0);
+    int i = 0;
+	int k;
+	int r;
+    while (message[k] != '\0')
+    {
+        i = 7;
+        while(i >= 0)
+        {
+			r = ((message[k] >> i) & 1);
+			printf("%d\n", r);
+            if (r)
+                kill(pid, SIGUSR1);
+            else
+                 kill(pid, SIGUSR2);
+            i--;
+			usleep(100);
+        }
+        k++;
+    }
+    return (0);
 }
 
 int main(int ac, char **av)
--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: user <mvaldeta@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 19:06:15 by user              #+#    #+#             */
-/*   Updated: 2021/10/29 22:14:53 by user             ###   ########.fr       */
+/*   Updated: 2021/10/29 23:00:09 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,54 +25,50 @@
            };
             */
 
+// int sig_sethandler(int sig, void *handler)
+// {
+//     struct sigaction *act;
+//     act = malloc(sizeof(struct sigaction));
+//     act->sa_sigaction = handler;
+//     act->sa_flags = SA_SIGINFO;
 
-void	*ft_memset(void *b, int c, size_t len)
-{
-	size_t i;
-
-	i = 0;
-	while (i < len)
-	{
-		((unsigned char *)b)[i] = c;
-		i++;
-	}
-	return (b);
-}
-
-int sig_sethandler(int sig, void *handler)
-{
-    struct sigaction *act;
-    act = malloc(sizeof(struct sigaction));
-    act->sa_sigaction = handler;
-    act->sa_flags = SA_SIGINFO;
-
-    sigaction(sig, act, NULL);
-    return (0);
-}
+//     sigaction(sig, act, NULL);
+//     return (0);
+// }
 
 void my_handler(int signum)
 {
-    static char buf[100];
-    static int i;
-    static int bit;
+    static char str[10];;;
+    char *endptr;
+    // str = malloc(9 * sizeof(char));
+   static char c = 0;
+    static int i = 0;
 
-    if (--bit == -1)
-    {
-        bit = 6;
-        ++i;
-    }
-    buf[i] &= ~(1 << 7);
-    if (signum == SIGUSR1)
-        buf[i] |= (1 << bit);
-    else if (signum == SIGUSR2)
-        buf[i] &= ~(1 << bit);
-    if (i == 99 || buf[i] == 127)
-    {
-        buf[i] = 0;
-        write(STDOUT_FILENO, buf, i + 1);
-        ft_memset(buf, '\0', 99);
+    // while (i < 8)
+    // {
+        if (signum == SIGUSR1)
+        {
+            str[i]= '1';
+            c += (1 << (7 -i));
+            // printf("Received SIGUSR1!\t%d\n", c);
+            //decode(message);
+        }
+        if (signum == SIGUSR2)
+        {
+            str[i]= '0';
+            // printf("Received SIGUSR2!\n");
+            //decode(message);
+        }
+        i++;
+    // }
+    // str[i] = '\0';
+    // c = strtol(str, (char **)NULL, 2);
+    //printf("c = %s = %c\n", str, c);
+    // printf("str = %s\ti %d\n", str, i);
+    if (i == 8) {
         i = 0;
-        bit = 0;
+    write(1, &c, 1);
+    c = 0;
     }
 }
 
@@ -85,10 +81,9 @@ int main(void)
     printf("pid: %d\n", pid);
     printf("...\n");
 
-    while (1)
-    {
         signal(SIGUSR1, my_handler);
         signal(SIGUSR2, my_handler);
+    while (1)
+    {
     }
-    //sleep(1000);
 }
